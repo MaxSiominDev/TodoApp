db015e13a27e205e06e03c2f76e94d93f4d17bfe
Maximsiomin <80629574+Maximsiomin@users.noreply.github.com>
2024-06-25 10:51:24 +0300

    buildSrc partially applied

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.File
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -8,4 +10,33 @@ plugins {
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.daggerHilt) apply false
     alias(libs.plugins.compose.compiler) apply false
+}
+
+
+tasks.register("generateVersionConstants") {
+    val outputDir = file("src/main/kotlin")
+    val outputFile = File(outputDir, "Versions.kt")
+
+    doLast {
+        val tomlFile = file("gradle/libs.versions.toml")
+        val tomlContent = tomlFile.readText()
+
+        val versionRegex = """(\w+)\s*=\s*"([^"]+)"""".toRegex()
+        val versions = versionRegex.findAll(tomlContent).map { matchResult ->
+            val (name, version) = matchResult.destructured
+            "const val ${name} = \"$version\""
+        }.joinToString("\n")
+
+        outputFile.writeText(
+            """
+            object Versions {
+                $versions
+            }
+            """.trimIndent()
+        )
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("generateVersionConstants")
 }
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    implementation("com.android.tools.build:gradle:8.5.0")
+}
